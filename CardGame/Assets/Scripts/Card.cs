e39c2b6bbfd37dbcbe2bcc38079b6371465a618a
0nichan23 <ontamam@gmail.com>
2021-11-06 20:46:35 +0200

    Card basic prefab and script

---
+++ CardGame/Assets/Scripts/Card.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card")]
+public class Card : ScriptableObject
+{
+    public string Name;
+    public string Description;
+    public Sprite ClassArt;
+    public Sprite TypeArt;
+
+
+}
---
+++ CardGame/Assets/Scripts/CardDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardDisplay : MonoBehaviour
+{
+    public Card RefCard;
+    public Text Name;
+    public Text Description;
+
+    public Image ClassArt;
+    public Image TypeArt;
+    void Start()
+    {
+        Name.text = RefCard.Name;
+        Description.text = RefCard.Description;
+        ClassArt.sprite = RefCard.ClassArt;
+        TypeArt.sprite = RefCard.TypeArt;
+    }
+
+}
