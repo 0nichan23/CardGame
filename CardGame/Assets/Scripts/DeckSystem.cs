c6f020801e55e75d508f57ef30907f8544429bba
0nichan23 <ontamam@gmail.com>
2021-11-15 22:05:22 +0200

    prefab changes

--- CardGame/Assets/Scripts/DeckSystem.cs
+++ CardGame/Assets/Scripts/DeckSystem.cs
@@ -2,25 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class DeckSystem : MonoBehaviour
 {
     public CardDisplay card;
     public static DeckSystem Instance { get; private set; }
-
+    public CardDisplay[] cards = new CardDisplay[5];
     public Stack<CardDisplay> PlayerGameDeck;
     public Stack<CardDisplay> PlayerWholeDeck;
+    
+    
 
     //value can be anything i want to be
 
     private void Awake()
     {
+        
         PlayerWholeDeck = new Stack<CardDisplay>();
         if (Instance == null)
         {
             Instance = this;
             for (int i = 0; i < 30; i++)
             {
-                PlayerWholeDeck.Push(card);
+                PlayerWholeDeck.Push(cards[Random.Range(0, cards.Length-1)]);
             }
             DontDestroyOnLoad(gameObject);
         }
